72cfbfd29474f7470a72496ee58250c16ce0d8a5
王迪 <wangdi05@yxqiche.com>
2018-11-12 10:52:06 +0800

    第一个版本

---
+++ Example/Pods/Target Support Files/Pods-WDTools_Tests/Pods-WDTools_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WDTools_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_WDTools_Tests
+@end
---
+++ Example/Pods/Target Support Files/WDTools/WDTools-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_WDTools : NSObject
+@end
+@implementation PodsDummy_WDTools
+@end
